e9adfd08a24469f1ca0594a6a1691b6d6d712552
Erandi Rajasekara <kalanika2013@gmail.com>
2020-11-30 18:01:38 +0530

    Remove wso2 license header

--- user-operation-event-listener-populate-displayName-claim/src/main/java/org/wso2/carbon/custom/user/operation/event/listener/CustomUserOperationEventListener.java
+++ user-operation-event-listener-populate-displayName-claim/src/main/java/org/wso2/carbon/custom/user/operation/event/listener/CustomUserOperationEventListener.java
@@ -1,21 +1,3 @@
-/*
- * Copyright (c) 2020, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
- *
- * WSO2 Inc. licenses this file to you under the Apache License,
- * Version 2.0 (the "License"); you may not use this file except
- * in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-
 package org.wso2.carbon.custom.user.operation.event.listener;
 
 import org.wso2.carbon.identity.core.AbstractIdentityUserOperationEventListener;
@@ -42,8 +24,8 @@ public class CustomUserOperationEventListener extends AbstractIdentityUserOperat
             return orderId;
         }
         // This listener should be executed before all the other listeners.
-        // 0 and 1 are already reserved for audit loggers, hence using 2.
-        return 2;
+        // 0 and 1 are already reserved for audit loggers, hence using 3.
+        return 3;
     }
 
     @Override
